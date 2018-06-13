8b92b17f4468891c45365dbdb5af2fce5b9beea4
Jordan Bice <jmbice@gmail.com>
2018-06-13 12:18:03 -0700

    Make stack pass all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,21 @@
 var Stack = function() {
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
-  // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    delete storage[count];
+    count--;
+    return storage[count];
   };
 
   someInstance.size = function() {
+    return count >= 0 ? count : 0;
   };
 
   return someInstance;
