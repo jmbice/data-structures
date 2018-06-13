7b48af3e985bb8993cff44bd14b69efdbf968b9f
Jordan Bice <jmbice@gmail.com>
2018-06-13 16:09:55 -0700

    Transform prototypal to pseudoclassical pattern
    Make all tests pass.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
 
+Stack.prototype.push = function(value){
+  this.count++;
+  var currentCount = this.count;
+  this[currentCount] = value;
+};
 
+Stack.prototype.pop = function(){
+  var currentDelete = this[this.count];
+  delete this[this.count];
+  this.count > 0 ? this.count-- : this.count = 0;
+  return currentDelete;
+};
+
+Stack.prototype.size = function(){
+  return this.count;
+};
