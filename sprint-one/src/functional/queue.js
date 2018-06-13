6400379675a5dcfc2f841e267fd4943247032b65
Jordan Bice <jmbice@gmail.com>
2018-06-13 12:15:53 -0700

    Make queue pass all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,24 @@
 var Queue = function() {
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
+  var count = 0;
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var lowest = Math.min(...Object.keys(storage));
+    var returnDeleted = storage[lowest];
+    delete storage[lowest];
+    count--;
+    return returnDeleted
+
   };
 
   someInstance.size = function() {
+    return count >= 0 ? count : 0;
   };
 
   return someInstance;
