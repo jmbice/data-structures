9fd519d80b2f318be3f3cb18726c438e0fea9fcc
Jordan Bice <jmbice@gmail.com>
2018-06-13 20:13:29 -0700

    Write queue ds class in prototypal style

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.count = 0;
+  return obj;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.count++;
+  var currentCount = this.count;
+  this[currentCount] = value;
+};
 
+queueMethods.dequeue = function() {
+  var lowest = Infinity;
+  for (keys in this) {
+    keys < lowest ? lowest = keys : null;
+  }
+  var returnDeleted = this[lowest];
+  delete this[lowest];
+  return returnDeleted;
+};
+
+queueMethods.size = function() {
+  var numOfKeys = 0;
+  var relevantObj = this;
+  for (var keys in relevantObj) {
+    numOfKeys++;
+  }
+  return numOfKeys - 4;
+};
