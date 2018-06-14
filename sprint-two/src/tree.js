c5e19a78969da7741dd0a7518b9bb33206d8212e
jmbice <jmbice@gmail.com>
2018-06-14 16:04:33 -0700

    Write methods for addChild and contains for tree class.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,31 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = { };
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var anObj = { 'value': value };
+  this.children.push(anObj);
 };
 
 treeMethods.contains = function(target) {
+  //iterate over this.children array
+  ///check each object in array and find "value" and check if it equals == target
+  //else return false
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].value === target) {
+      return true;
+    }
+  }
+  return false;
 };
-
 
 
 /*
