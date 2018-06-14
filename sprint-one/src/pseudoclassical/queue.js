7e4b5e3d288ed5d2e35d55e9ce8d9ad479c82a86
Jordan Bice <jmbice@gmail.com>
2018-06-13 20:16:31 -0700

    Write queue ds class using psuedoclassical style

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.count++;
+  var currentCount = this.count;
+  this[currentCount] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  var lowest = Infinity;
+  for (keys in this) {
+    keys < lowest ? lowest = keys : null;
+  }
+  var returnDeleted = this[lowest];
+  delete this[lowest];
+  return returnDeleted;
+};
+
+Queue.prototype.size = function() {
+  var numOfKeys = 0;
+  var relevantObj = this;
+  for (var keys in relevantObj) {
+    numOfKeys++;
+  }
+  return numOfKeys - 4;
+};
