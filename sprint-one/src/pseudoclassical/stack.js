35938f9d7f092424398fbfc2cafcf9d4da46bc8d
alilthumuch <alilthumuch@gmail.com>
2018-06-14 02:04:44 -0700

    Pseudoclasslical Stack function pass

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+
+Stack.prototype.push = function(value) {
+  	// console.log("does execute")
+  	// console.log(this, "passing value")
+  	var index = Object.keys(this.storage).length;
+  	this.storage[index] = value;
+};
+Stack.prototype.pop =  function() {
+  	var lastIndex = Object.keys(this.storage).length-1;
+  	var lastElement = this.storage[lastIndex];
+  	delete this.storage[lastIndex];
+  	return lastElement;
+};
+Stack.prototype.size =  function() {
+  	// console.log("test");
+  	var length = Object.keys(this.storage).length;
+  	return length;
+};
+
+new Stack();
+
 
 
