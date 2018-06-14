6c86c324b74597b308bdb1103ba6a52c63b192ab
Jordan Bice <jmbice@gmail.com>
2018-06-13 20:23:26 -0700

    Write queue ds class using es6 style

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,28 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
   }
 
+  enqueue(value) {
+    this.count++;
+    this[this.count] = value;
+  }
+
+  dequeue() {
+    var lowest = Infinity;
+    for (var keys in this) {
+      keys < lowest ? lowest = keys : null;
+    }
+    var returnDeleted = this[lowest];
+    delete this[lowest];
+    return returnDeleted;
+  }
+
+  size() {
+    var numOfKeys = 0;
+    for (var keys in this) {
+      numOfKeys++;
+    }
+    return numOfKeys - 1;
+  }
 }
