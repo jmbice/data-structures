68355dd86ac674a61750195122c16ab1a8a561b2
alilthumuch <alilthumuch@gmail.com>
2018-06-14 00:59:33 -0700

    Stack functional shared pattern pass

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,54 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  // stackMethods.push = function(value) {
+  // 	console.log("does execute")
+  // 	var index = Object.key(storage).length;
+  // 	storage[index] = value
+  // }
+
+  // stackMethods.pop = function() {
+  // 	var lastIndex = Object.key(storage).length-1;
+  // 	var lastElement = storage[lastIndex];
+  // 	delete storage[lastIndex];
+  // 	return lastElement;
+  // }
+
+  // stackMethods.size = function() {
+  // 	var length = Object.key(storage).length;
+  // 	return length;
+  // }
+  _.extend(instance, stackMethods);
+  //console.log(storage, "storage");
+  return instance;
+
 };
 
-var stackMethods = {};
+
+stackMethods = {};
+
+//var stackMethods = {
+stackMethods.push = function(value) {
+  	console.log("does execute")
+  	console.log(this, "passing value")
+  	var index = Object.keys(this.storage).length;
+  	this.storage[index] = value;
+};
+stackMethods.pop =  function() {
+  	var lastIndex = Object.keys(this.storage).length-1;
+  	var lastElement = this.storage[lastIndex];
+  	delete this.storage[lastIndex];
+  	return lastElement;
+};
+stackMethods.size =  function() {
+  	console.log("test");
+  	var length = Object.keys(this.storage).length;
+  	return length;
+};
+
+
+
 
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,22 +3,16 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var last = "";
-
+  // var last = "";
   // Implement the methods below
   someInstance.push = function(value) {
     //someInstance {push: function(value)}
-    var index = Object.keys(storage).length 
-
+    var index = Object.keys(storage).length; 
     storage[index] = value;
-    last = value;
+    // last = value;
 
   };
-
   someInstance.pop = function() {
-    //someInstance {pop: function(value)}
-    //delete storage[value]
-    //find last item on storage and pop it out
     var key = storage[Object.keys(storage).length-1];
     delete storage[Object.keys(storage).length-1];
     return key;
@@ -27,14 +21,6 @@ var Stack = function() {
   someInstance.size = function() {
     var length = Object.keys(storage).length;
     return length;
-    //someInstance {size: function}
-    // var size = function(storage){
-    //   var count = 0
-    //   for (key in someInstance) {
-    //     count +=
-    //   }
-    // }
-    // return count;
   };
 
   return someInstance;
