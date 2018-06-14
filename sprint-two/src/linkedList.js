ade8b1c870071d69a10222fc8655f741c29df208
jmbice <jmbice@gmail.com>
2018-06-14 15:40:47 -0700

    LinkedList class to pass test.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,36 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function(value) { 
+    if (list.tail === null) {
+      var first = Node(value);
+      list.head = first;
+      list.tail = first;
+    } else {
+      var newNode = Node(value);
+      list.head.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var wasNextHead = list.head.next;
+    var wasPrevHead = list.head.value;
+    list.head = wasNextHead;
+    return wasPrevHead;
   };
 
   list.contains = function(target) {
+    var contain = function(target, collection) {
+      if (collection.value === target) {
+        return true;
+      } else if (collection.next === null) {
+        return false;
+      } else {
+        return contain(target, collection.next);
+      }
+    };
+    return contain(target, list.head);
   };
 
   return list;
